e50540362e507e1006ad0308da8f439cd7a83327
Sanjay Pathak <forjoyonly289@gmail.com>
2024-06-23 19:44:34 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -9,82 +9,83 @@ const OAuth2 = google.auth.OAuth2;
 const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
+const { job } = require("./cron.js");
 
-const publicPath = path.join(__dirname,"/public");
+const publicPath = path.join(__dirname, "/public");
 app.use(express.static(publicPath));
-const partialPath = path.join(__dirname,"/partials");
+const partialPath = path.join(__dirname, "/partials");
 hbs.registerPartials(partialPath);
 
 app.set("view engine", "hbs")
 app.use(express.urlencoded());
 app.use(express.json());
-
-app.get("/",(req,res)=>{
-    res.render("index")
+job();
+app.get("/", (req, res) => {
+  res.render("index")
 })
 
-app.get("/contact",(req,res)=>{
+app.get("/contact", (req, res) => {
 
-    res.render("contactPage");
+  res.render("contactPage");
 })
 
-app.post("/contact",(req,res)=>{
-    const createTransporter = async () => {
-        const oauth2Client = new OAuth2(
-          process.env.CLIENT_ID,
-          process.env.CLIENT_SECRET,
-          "https://developers.google.com/oauthplayground"
-        );
-      
-        oauth2Client.setCredentials({
-          refresh_token: process.env.REFRESH_TOKEN
-        });
-      
-        const accessToken = await new Promise((resolve, reject) => {
-          oauth2Client.getAccessToken((err, token) => {
-            if (err) {
-              console.log(err);
-            }
-            resolve(token);
-          });
-        });
-      
-        const transporter = nodemailer.createTransport({
-          service: "gmail",
-          auth: {
-            type: "OAuth2",
-            user: process.env.EMAIL,
-            accessToken,
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            refreshToken: process.env.REFRESH_TOKEN
-          }, 
-          tls : {
-            rejectUnauthorized : false
-          }
-        });
-        return transporter;
-      
-      };
+app.post("/contact", (req, res) => {
+  const createTransporter = async () => {
+    const oauth2Client = new OAuth2(
+      process.env.CLIENT_ID,
+      process.env.CLIENT_SECRET,
+      "https://developers.google.com/oauthplayground"
+    );
 
-      const sendmail = async (mailOptions) => {
-        let emailTransporter = await createTransporter();
-        await emailTransporter.sendMail(mailOptions);
+    oauth2Client.setCredentials({
+      refresh_token: process.env.REFRESH_TOKEN
+    });
+
+    const accessToken = await new Promise((resolve, reject) => {
+      oauth2Client.getAccessToken((err, token) => {
+        if (err) {
+          console.log(err);
+        }
+        resolve(token);
+      });
+    });
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.EMAIL,
+        accessToken,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN
+      },
+      tls: {
+        rejectUnauthorized: false
       }
-      
-      sendmail({
-        subject: `Message from a website visitor - ${req.body.name}`,
-        text: `${req.body.email}`+ `\n` + req.body.message,
-        from: req.body.email,
-        to: process.env.EMAIL,
-        
-      })
-      res.redirect("/contact");
+    });
+    return transporter;
+
+  };
+
+  const sendmail = async (mailOptions) => {
+    let emailTransporter = await createTransporter();
+    await emailTransporter.sendMail(mailOptions);
+  }
+
+  sendmail({
+    subject: `Message from a website visitor - ${req.body.name}`,
+    text: `${req.body.email}` + `\n` + req.body.message,
+    from: req.body.email,
+    to: process.env.EMAIL,
+
+  })
+  res.redirect("/contact");
 })
 
-app.get("*", (req,res) => {
+app.get("*", (req, res) => {
   res.render("404")
 })
-app.listen(port,(req,res)=>{
-    console.log("Server Started");
+app.listen(port, (req, res) => {
+  console.log("Server Started");
 });
